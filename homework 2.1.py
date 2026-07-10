e50046a024957abae3a6a18d3a7a5357ea250d60
denisdzezela <dzhezhela.denis8@gmail.com>
2026-07-10 04:01:39 +0200

    Write the numbers from right to left

--- homework 2.1.py
+++ homework 2.1.py
@@ -1,15 +1,15 @@
 numbers_1 = int(input("Enter the first number: "))
-numebrs_2 = input("Enter the operation:")
+numbers_2 = input("Enter the operation:")
 numebrs_3 = int(input("Enter the second number:"))
 
 
-if numebrs_2 == "+":
+if numbers_2 == "+":
     print(int(numbers_1 + numebrs_3))
-elif numebrs_2 == "-":
+elif numbers_2 == "-":
     print(numbers_1 - numebrs_3)
-elif numebrs_2 == "*":
+elif numbers_2 == "*":
     print(numbers_1 * numebrs_3)
-elif numebrs_2 == "/" and numebrs_3 != 0:
+elif numbers_2 == "/" and numebrs_3 != 0:
     print(numbers_1 / numebrs_3)
 else:
     print("Cannot divide by zero")
