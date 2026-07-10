04b99255b5afa92bddc1a99c3c36a6447cb3ff23
denisdzezela <dzhezhela.denis8@gmail.com>
2026-07-10 03:40:13 +0200

    Write the numbers from right to left

---
+++ homework 2.1 in one line.py
@@ -0,0 +1,6 @@
+numbers_1 = int(input("Enter the first number: "))
+numbers_2 = input("Enter the operation:")
+numbers_3 = int(input("Enter the second number:"))
+
+result = ( numbers_1 + numbers_3) if numbers_2 == "+" else ( (numbers_1 - numbers_3) if numbers_2 == "-" else ((numbers_1 * numbers_3) if numbers_2 == "*" else (( numbers_1 / numbers_3) )))
+print(result)
