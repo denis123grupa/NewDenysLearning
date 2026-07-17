0cae4c4083f9c56902af1ef6e3ea36d660b9336d
denisdzezela <dzhezhela.denis8@gmail.com>
2026-07-17 21:31:54 +0200

    Write the numbers from right to left

---
+++ homework 3.1.py
@@ -0,0 +1,15 @@
+my_list = [0, 1, 0, 12, 3, 0]
+new_list = []
+
+for new in my_list:
+   if new == 0:
+       continue
+   new_list.append(new)
+
+
+zero = my_list.count(0)
+
+for x in range(zero):
+    new_list.append(0)
+
+print(new_list)
