2b0026dac8f57d5a2930952f064ab72a6331d804
denisdzezela <dzhezhela.denis8@gmail.com>
2026-07-25 10:50:08 +0200

    Write the numbers from right to left

--- homework 3.2.py
+++ homework 3.2.py
@@ -1,21 +1,27 @@
 
 
 
-my_list = [0, 1, 12, 1, 1, 5]
+# my_list = [0, 1, 12, 1, 1, 5]
+my_list = []
 
 new_list = []
 
-for x in range(0, len(my_list), 2):
-  new_list.append(my_list[x])
+if my_list == []:
 
-summa = 0
+    print(0)
+else:
 
-for z in range(len(new_list)):
-    summa += (new_list[z])
-times = my_list[-1]
+    for x in range(0, len(my_list), 2):
+      new_list.append(my_list[x])
 
-result = summa * times
-print(result)
+    summa = 0
+
+    for z in range(len(new_list)):
+        summa += (new_list[z])
+    times = my_list[-1]
+
+    result = summa * times
+    print(result)
 
 
 
