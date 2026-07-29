4aa1ec5407fcbb115c89ff71ddb59bdc9d81b5ac
denisdzezela <dzhezhela.denis8@gmail.com>
2026-07-29 18:22:09 +0200

    Write the numbers from right to left

--- homework 3.3.py
+++ homework 3.3.py
@@ -1,10 +1,21 @@
 
+# first vesrsion
 
-my_list = [4, 3, 5, 6, 9]
+# my_list = [4, 3, 5, 6, 9]
+#
+# new_list = []
+#
+# new_list = [my_list[0]]
+# new_list.append(my_list[2])
+# new_list.append(my_list[-2])
+# print(new_list)
+
+# secind version
+
+my_list = [4, 3, 5]
 
 new_list = []
 
-new_list = [my_list[0]]
-new_list.append(my_list[2])
-new_list.append(my_list[-2])
+new_list = [my_list[0], my_list[2], my_list[-2]]
+
 print(new_list)
