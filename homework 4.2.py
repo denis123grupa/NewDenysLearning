2e3cfd7adb64c2f6e808db7a5027d4ba6c7ed098
denisdzezela <dzhezhela.denis8@gmail.com>
2026-08-09 03:49:42 +0200

    Write the numbers from right to left

--- homework 4.2.py
+++ homework 4.2.py
@@ -11,16 +11,20 @@ new_result = []
 
 import string
 
-result = first_adress.split(" ")
+
+for x in first_adress:
+    if x not in string.punctuation:
+        result += x
+
+result = result.split(" ")
 for a in result:
     new_result.append(a.capitalize())
 result = "".join(new_result)
-for x in first_adress:
-    if x not in string.punctuation and x == " ":
-        result += x
+
 result = f"{hashtag}{result}"
 
-
+if len(result) > 140:
+    result = result[:140]
 
 
 print(result)
