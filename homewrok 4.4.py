29d3a56cf07a4a1464bbafcfd158c1857aaad4e1
denisdzezela <dzhezhela.denis8@gmail.com>
2026-08-10 22:34:45 +0200

    Write the numbers from right to left

--- homewrok 4.4.py
+++ homewrok 4.4.py
@@ -1,6 +1,8 @@
 
+original_name = input(str(" Hi, my friend! I’ll help you encrypt the text. Please enter the name: "))
 
-original_name = input(str("Enter the name:"))
+
+
 count = []
 count_2 = []
 value_1 = []
@@ -11,16 +13,26 @@ decrypted_name = ""
 
 
 for x in original_name:
-    count = ord(x) + 3
+    count = ord(x) + 100
     count_2 = chr(count)
     encrypted_name += str(count_2)
-
 print(encrypted_name)
 
-for y in encrypted_name:
-    value_1 = ord(y) - 3
-    value_2 = chr(value_1)
-    decrypted_name += str(value_2)
+ok = input(str("Do you want to decrypt the name? If yes, enter yes. If not, enter no: "))
+
+if ok == "yes":
+
+    for y in encrypted_name:
+        value_1 = ord(y) - 100
+        value_2 = chr(value_1)
+        decrypted_name += str(value_2)
+
+    print(decrypted_name)
+else:
+    print(f" Then let’s leave it encrypted:   {encrypted_name}")
 
 
-print(decrypted_name)
+
+
+
+
