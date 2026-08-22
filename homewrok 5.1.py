b8977ce52deb704c76da2122882f08420f59ae9a
denisdzezela <dzhezhela.denis8@gmail.com>
2026-08-22 18:18:58 +0200

    Write the numbers from right to left

--- homewrok 5.1.py
+++ homewrok 5.1.py
@@ -1,7 +1,9 @@
 
 import string
 
+
 value = input(str(" Enter your value in this format: a-d :"))
+
 
 all_value = string.ascii_letters
 string_value = ""
@@ -20,6 +22,8 @@ index_start_symbol = all_value.index(start_symbol)
 index_last_symbol = all_value.index(last_symbol)
 
 string_value = all_value[index_start_symbol:index_last_symbol + 1]
+
+
 
 if index_start_symbol > index_last_symbol or string_value not in all_value:
     print("Not in alphabetical order")
