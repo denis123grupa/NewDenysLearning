3e09f4ef0e7cf7012dd7b8c4ced21bd2c394d10d
denisdzezela <dzhezhela.denis8@gmail.com>
2026-09-02 04:12:13 +0200

    Write the numbers from right to left

--- homework 5.3.py
+++ homework 5.3.py
@@ -1,16 +1,30 @@
 
+def cal():
+    number = int(input("Enter your namber :"))
+    while number > 9:
+        new = 1
+        for y in str(number):
+            new = new * int(y)
+
+        number = new
+    print(new)
+
+cal()
 
 
-number = int(input("Enter your namber :"))
 
 
-while number > 9:
-    new = 1
-    for y in str(number):
-        new = new * int(y)
+# number = int(input("Enter your namber :"))
+#
+#
+# while number > 9:
+#     new = 1
+#     for y in str(number):
+#         new = new * int(y)
+#
+#     number = new
+#
+# print(new)
 
-    number = new
-
-print(new)
 
 
