38dfe53c23d9bfda9de016a9b9c58724e63798bf
denisdzezela <dzhezhela.denis8@gmail.com>
2026-09-10 22:30:25 +0200

    Write the numbers from right to left

--- homework 7.1.py
+++ homework 7.1.py
@@ -2,12 +2,12 @@
 
 def add_one(some_list):
     new = ""
-    for x in some_list:
-        new += str(x)
-    new_1 = str(int(new) + 1)
-    new_2 = [int(y) for y in new_1]
+    for some in some_list:
+        new += str(some)
+    some_string = str(int(new) + 1)
+    new_some_list = [int(y) for y in some_string]
 
-    return new_2
+    return new_some_list
 
 result = add_one([1, 2, 3, 4, 5])
 
@@ -16,3 +16,23 @@ assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
 assert add_one([0]) == [1], 'Test3'
 assert add_one([9]) == [1, 0], 'Test4'
 print("ОК")
+
+
+##### metod 2 ######
+#
+# def add_one(some_list):
+#
+#     new_list = [str(number) for number in some_list]
+#     new_string = str(int("".join(new_list)) + 1)
+#     result = [int(x) for x in new_string]
+#
+#     return  result
+# #
+# print(add_one([1, 2, 3, 4]))
+# #
+#
+# assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test1'
+# assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test2'
+# assert add_one([0]) == [1], 'Test3'
+# assert add_one([9]) == [1, 0], 'Test4'
+# print("ОК")
