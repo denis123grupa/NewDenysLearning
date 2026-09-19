4302125d3c908005149ffb82193b76cbdead48c6
denisdzezela <dzhezhela.denis8@gmail.com>
2026-09-19 18:54:53 +0200

    Write the numbers from right to left

--- homework 7.3.py
+++ homework 7.3.py
@@ -1,15 +1,38 @@
 
 
+####### metod 1 #######
+
+
 def find_unique_value(n):
+    from decimal import Decimal
 
-    for value in n:
-        summ_value = n.count(value)
+    n = [value_1 for value_1 in n if isinstance(value_1, (int, float, Decimal))]
+    for value_2 in n:
+        summ_value = n.count(value_2)
         if summ_value == 1:
-         return value
+         return value_2
 
-result = find_unique_value([1, 2, 1, 1])
+result = find_unique_value([1, 2, 1, 1, "Hello", [1, 2, 3]])
 
 assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
 assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
 assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
 print("ОК")
+
+
+####### metod 2 #######
+
+#
+# def find_unique_value(n):
+#
+#     for value in n:
+#         summ_value = n.count(value)
+#         if summ_value == 1:
+#          return value
+#
+# result = find_unique_value([1, 2, 1, 1])
+#
+# assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
+# assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
+# assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
+# print("ОК")
